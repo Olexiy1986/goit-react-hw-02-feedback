4dff19e709a64220e9d957272d11e5ba24aea9df
Oleksiy Shovkun <ssapfir1986@ukr.net>
2020-06-23 21:38:20 +0300

    Updates

---
+++ precache-manifest.3586ba462bbe7a6a8033b7774ae0d571.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "563d3ab29498585bfe421d392efc7726",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "cb559928f11a61566f26",
+    "url": "/goit-react-hw-02-feedback/static/css/main.3bfab56e.chunk.css"
+  },
+  {
+    "revision": "ee17cd1a0f125a860ef1",
+    "url": "/goit-react-hw-02-feedback/static/js/2.8c641c3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.8c641c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb559928f11a61566f26",
+    "url": "/goit-react-hw-02-feedback/static/js/main.fd38f37b.chunk.js"
+  },
+  {
+    "revision": "67fc603d4fe28264fbd4",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.56a9b6a1.js"
+  }
+]);
